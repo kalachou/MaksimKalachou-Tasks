20b7dbb0f6d35d65e1d72fe68499568188833e99
kalachou <mvkalach>
2018-11-02 00:23:21 +0300

    fix(task4.1): implement right Atkin sieve algorithm

--- Task4/1/task4_1.js
+++ Task4/1/task4_1.js
@@ -1,33 +1,37 @@
 module.exports = function getFirstPrimesLessThanN(n) {
-  const sqrt = Math.sqrt(n) + 1;// +1 for cases <= to use < instead
-  const compareN = n + 1;// for cases <= to use < instead
-  const isprime = new Uint32Array(n + 1);
+  const sqrt = Math.ceil(Math.sqrt(n));// +1 for cases <= to use < instead
+  const isPrime = new Uint8Array(n);
   let i2 = 0;
   let j2;
+  let num;
+  let i, j, s;
 
-  for (let i = 1; i < sqrt; i++) {
+  for (i = 1; i < sqrt; i++) {
     i2 += 2 * i - 1;
     j2 = 0;
-    for (let j = 1; j < sqrt; j++) {
+    for (j = 1; j < sqrt; j++) {
       j2 += 2 * j - 1;
-      let num = 4 * i2 + j2;
-      if ((num < n) && (num % 12 === 1 || num % 12 === 5)) isprime[num] = num;
+      num = 4 * i2 + j2;
+      if ((num < n) && (num % 12 === 1 || num % 12 === 5)) isPrime[num] = isPrime[num] ? 0 : 1;
       num -= i2;
-      if ((num < compareN) && (num % 12 === 7)) isprime[num] = num;
+      if ((num < n) && (num % 12 === 7)) isPrime[num] = isPrime[num] ? 0 : 1;
       num -= 2 * j2;
-      if ((i > j) && (num <= n) && (num % 12 == 11)) isprime[num] = num;
+      if ((i > j) && (num < n) && (num % 12 === 11)) isPrime[num] = isPrime[num] ? 0 : 1;
     }
   }
-  for (let i = 5; i < sqrt; i++) {
-    if (isprime[i]) {
-      const s = i * i;
-      for (let j = s; j < compareN; j += s) isprime[j] = false;
+  for (i = 5; i < sqrt; i++) {
+    if (isPrime[i]) {
+      s = i * i;
+      for (j = s; j < n; j += s) isPrime[j] = 0;
     }
   }
-  isprime[2] = 2;
-  isprime[3] = 3;
+  isPrime[2] = 1;
+  isPrime[3] = 1;
 
-  return isprime.filter((v, i) => !!v && (i === 3 || i % 3 !== 0) && (i === 5 || i % 5 !== 0));
+return isPrime.reduce((p, v, i) =>
+            { if (!!v 
+              && (i === 3 || i % 3 !== 0) 
+              && (i === 5 || i % 5 !== 0)) p.push(i); return p; }, []);
 };
 /*
 function getNFirstPrimes (n) {

8fb34f0066be382a330088b88681ba1b9284d942
kalachou <mvkalach>
2018-11-02 01:12:24 +0300

    feat(task4_2): correct sieve of Atkin algorithm, add estimation of limit for nth prime number range

--- Task4/2/task4_2.js
+++ Task4/2/task4_2.js
@@ -1,8 +1,7 @@
 module.exports = function getNFirstPrimes(n) {
-  const limit = n * 8;
-  const sqrt = Math.sqrt(limit) + 1;// +1 for cases <= to use < instead
-  const compareN = limit + 1;// for cases <= to use < instead
-  const isprime = new Uint32Array(limit + 1);
+  const limit = n > 20 ? Math.ceil(n * (Math.log(n) + Math.log(Math.log(n)) - 1 / 2)) : 72;
+  const sqrt = Math.ceil(Math.sqrt(limit));// +1 for cases <= to use < instead
+  const isprime = new Uint8Array(limit);
   let i2 = 0;
   let j2;
 
@@ -12,26 +11,28 @@ module.exports = function getNFirstPrimes(n) {
     for (let j = 1; j < sqrt; j++) {
       j2 += 2 * j - 1;
       let num = 4 * i2 + j2;
-      if ((num < limit) && (num % 12 === 1 || num % 12 === 5)) isprime[num] = num;
+      if ((num < limit) && (num % 12 === 1 || num % 12 === 5)) isprime[num] = isprime[num] ? 0 : 1;
       num -= i2;
-      if ((num < compareN) && (num % 12 === 7)) isprime[num] = num;
+      if ((num < limit) && (num % 12 === 7)) isprime[num] = isprime[num] ? 0 : 1;
       num -= 2 * j2;
-      if ((i > j) && (num < compareN) && (num % 12 == 11)) isprime[num] = num;
+      if ((i > j) && (num < limit) && (num % 12 === 11)) isprime[num] = isprime[num] ? 0 : 1;
     }
   }
+
   for (let i = 5; i < sqrt; i++) {
     if (isprime[i]) {
       const s = i * i;
-      for (let j = s; j < compareN; j += s) isprime[j] = false;
+      for (let j = s; j < limit; j += s) isprime[j] = false;
     }
   }
-  isprime[2] = 2;
-  isprime[3] = 3;
+
+  isprime[2] = 1;
+  isprime[3] = 1;
 
   return isprime
-    .filter((v, i) => 
-        !!v 
+    .reduce((p, v, i) => 
+        { if (!!v 
         && (i === 3 || i % 3 !== 0) 
-        && (i === 5 || i % 5 !== 0))
+        && (i === 5 || i % 5 !== 0)) p.push(i); return p; }, [])
     .slice(0, n);
 };

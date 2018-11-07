2bb3648bc7f052068193faa26405fb4e1552eabc
kalachou <mvkalach>
2018-11-07 08:46:32 +0300

    (task_4): remove temp variable

--- Task4/3/task4_3.js
+++ Task4/3/task4_3.js
@@ -1,13 +1,10 @@
 module.exports = function getSumFirstEvenFibonacci(NumberOfEventFibonacci) {
   let first = 1;
   let second = 1;
-  let temp;
   const list = [];
 
   while (list.length < NumberOfEventFibonacci) {
-    temp = first;
-    first = second;
-    second += temp;
+    [first, second] = [second, first + second];
     if (second % 2 === 0) list.push(second);
   }
   return list.reduce((previous, value) => previous + value);
